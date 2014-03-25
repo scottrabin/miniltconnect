202a1562fb585692b10dd8611b65cd8cfc3b1408
Scott Rabin <scott@huddler-inc.com>
2014-03-25 16:25:56 -0700

    Persist enabled domains and auto-connect/disconnect as needed

---
+++ background.js
@@ -0,0 +1,219 @@
+// extension cached configs
+var port = null;
+var enabledDomains = {};
+var lastActiveTabId = null;
+
+// page scripts for connecting/disconnecting from LT
+var LT_WS_CONNECT = ["(function() {",
+                     "function notify_connect() {",
+                       "console.log('LightTable Connect: connected @', {{ host }});",
+                     "}",
+                     "if (window.io && window.io.sockets && window.io.sockets[{{ host }}]) {",
+                       "if (!window.io.sockets[{{ host }}].connected) {",
+                         "window.io.sockets[{{ host }}].connect();",
+                         "notify_connect();",
+                       "}",
+                     "} else {",
+                       "var ws_script = document.createElement('script');",
+                       "ws_script.src = {{ host }} + '/socket.io/lighttable/ws.js';",
+                       "ws_script.id = 'lt_ws';",
+                       "ws_script.onload = notify_connect;",
+                       "ws_script.onerror = function() { console.error('LightTable Connect: Unable to connect @', {{ host }}); };",
+                       "document.head.appendChild(ws_script);",
+                     "}",
+                     "})();"
+                    ].join('');
+var LT_WS_DISCONNECT = ["(function() {",
+                        "if (window.io && window.io.sockets && window.io.sockets[{{ host }}]) {",
+                          "window.io.sockets[{{ host }}].disconnect();",
+                          "console.log('LightTable Connect: disconnected @', {{ host }});",
+                        "}",
+                        "})();"
+                       ].join('');
+
+var SCRIPT_EXECUTION_TEMPLATE = ["(function() {",
+                                 "var scr = document.createElement('script');",
+                                 "scr.innerHTML = {{ code }};",
+                                 "document.head.appendChild(scr);",
+                                 "})();"
+                                ].join('');
+
+/**
+ * Set the LightTable connection port
+ *
+ * @param {Number} port
+ */
+function setPort(port) {
+  var newVal = parseInt(port, 10);
+  if (!isNaN(newVal)) {
+    port = newVal;
+    chrome.storage.local.set({port: newVal});
+  }
+}
+
+/**
+ * Get the currently configured port value
+ *
+ * @return {Number}
+ */
+function getPort() {
+  return port;
+}
+
+/**
+ * Add a hostname to the set of hostnames LT should automatically connect to
+ *
+ * @param {String} hostname
+ */
+function addHostname(hostname) {
+  if (!enabledDomains[hostname]) {
+    enabledDomains[hostname] = true;
+    chrome.storage.local.set({"hostnames": enabledDomains});
+  }
+}
+
+/**
+ * Remove a hostname from the set of hostnames LT should automatically connect to
+ *
+ * @param {String} hostname
+ */
+function removeHostname(hostname) {
+  if (enabledDomains[hostname]) {
+    delete enabledDomains[hostname];
+    chrome.storage.local.set({"hostnames": enabledDomains});
+  }
+}
+
+/**
+ * Determine if a hostname has LT connect enabled
+ *
+ * @param {String} hostname
+ * @return {Boolean}
+ */
+function isHostnameEnabled(hostname) {
+  return enabledDomains[hostname] || false;
+}
+
+/**
+ * Parse the hostname from a string
+ *
+ * @param {String} url
+ * @return {String}
+ */
+function getHostname(url) {
+  return url.split('//')[1].split('/')[0];
+}
+
+/**
+ * Substitute {{ }}-delimited placeholders with values from a given context object
+ *
+ * @param {String} template
+ * @param {Object} obj
+ * @return {String}
+ */
+function sub(template, obj) {
+  return template.replace(/{{\s*(\w+)\s*}}/g, function(_, param) {
+    return obj[param] || "";
+  });
+}
+
+/**
+ * Execute the given code in the evaluation context of the page,
+ * because Chrome has a stringent execution policy that otherwise
+ * prevents calling functions defined on a given page
+ *
+ * @param {Number?} tabId The id of the tab to execute the code in
+ * @param {String} code
+ * @param {Function?} callback
+ */
+function executeScriptInPageContext(tabId, code, callback) {
+  chrome.tabs.executeScript(tabId, {
+    code: sub(SCRIPT_EXECUTION_TEMPLATE, {code: JSON.stringify(code)})
+  }, callback);
+}
+
+/**
+ * Toggle the LT connection to the specified tab
+ *
+ * @param {Number} tabId
+ * @param {Boolean} enable
+ */
+function toggleTabConnection(tabId, enable) {
+  // @#$%ing security policy requires we insert script contents
+  // that need to access the page variable (e.g. window.io) by
+  // actually inserting a <script> element with the contents as needed...
+  var template = (enable ? LT_WS_CONNECT : LT_WS_DISCONNECT);
+  executeScriptInPageContext(tabId, sub(template, {host: JSON.stringify('http://localhost:' + port)}), function() {
+    if (enable) {
+      lastActiveTabId = tabId;
+    } else if (lastActiveTabId === tabId) {
+      lastActiveTabId = null;
+    }
+  });
+}
+
+/**
+ * Connect to the given tab
+ *
+ * @param {Number} tabId
+ */
+function connectToTab(tabId) {
+  if (lastActiveTabId !== tabId) {
+    if (lastActiveTabId) {
+      toggleTabConnection(lastActiveTabId, false);
+    }
+    toggleTabConnection(tabId, true);
+  }
+}
+
+// when a tab updates, connect or disconnect as needed
+chrome.tabs.onUpdated.addListener(function(tabId, props) {
+  if (props.hasOwnProperty('url')) {
+    // url changed, connect if it is now enabled, or disconnect otherwise
+    var hostname = getHostname(props.url);
+    if (isHostnameEnabled(hostname)) {
+      connectToTab(tabId);
+    } else if (lastActiveTabId === tabId) {
+      toggleTabConnection(tabId, false);
+    }
+  } else if (props.hasOwnProperty('status')) {
+    if (props.status === 'loading') {
+      if (lastActiveTabId === tabId) {
+        // user refreshed the page, most likely...
+        lastActiveTabId = null;
+      }
+    } else if (props.status === 'complete') {
+      // tab just became ready, try to connect if necessary
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        if (tabs[0].active && isHostnameEnabled(getHostname(tabs[0].url))) {
+          connectToTab(tabs[0].id);
+        }
+      });
+    }
+  }
+});
+
+// when the focused tab changes, attempt to connect to it if
+// it falls under the set of domains that are flagged as "auto connect"
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab) {
+    if (isHostnameEnabled(getHostname(tab.url))) {
+      connectToTab(tab.id);
+    }
+  });
+});
+
+// load the stored values
+chrome.storage.local.get(null, function(vals) {
+  setPort(vals.port);
+  enabledDomains = vals.hostnames;
+});
+
+// Public API
+window.addHostname = addHostname;
+window.removeHostname = removeHostname;
+window.getHostname = getHostname;
+window.isHostnameEnabled = isHostnameEnabled;
+window.setPort = setPort;
+window.connectToTab = connectToTab;
+window.toggleTabConnection = toggleTabConnection;
--- popup.js
+++ popup.js
@@ -1,68 +1,10 @@
-// extension cached configs
-var port = null;
-var enabledDomains = {};
+// API
+var ltconn = chrome.extension.getBackgroundPage();
 
 // popup elements
 var form = document.getElementById('config');
 var portField = document.getElementById('config_port');
 var enableField = document.getElementById('config_enable');
-var hostnameField = document.getElementById('config_hostname');
-
-var LT_WS_CONNECT = ["(function() {",
-                     "if (window.io && window.io.sockets && window.io.sockets[{{ host }}]) {",
-                       "window.io.sockets[{{ host }}].connect();",
-                     "} else {",
-                       "var ws_script = document.createElement('script');",
-                       "ws_script.src = {{ host }} + '/socket.io/lighttable/ws.js';",
-                       "ws_script.id = 'lt_ws';",
-                       "document.head.appendChild(ws_script);",
-                     "}",
-                     "console.log('LightTable Connect: connected @', {{ host }});",
-                     "})();"
-                    ].join('');
-var LT_WS_DISCONNECT = ["(function() {",
-                        "if (window.io && window.io.sockets && window.io.sockets[{{ host }}]) {",
-                          "window.io.sockets[{{ host }}].disconnect();",
-                          "console.log('LightTable Connect: disconnected @', {{ host }});",
-                        "}",
-                        "})();"
-                       ].join('');
-
-var SCRIPT_EXECUTION_TEMPLATE = ["(function() {",
-                                 "var scr = document.createElement('script');",
-                                 "scr.innerHTML = {{ code }};",
-                                 "document.head.appendChild(scr);",
-                                 "})();"
-                                ].join('');
-
-function setPort(v) {
-  var newVal = parseInt(v, 10);
-  if (!isNaN(newVal)) {
-    portField.value = newVal;
-    port = newVal;
-    chrome.storage.local.set({port: newVal});
-  }
-}
-
-function sub(template, obj) {
-  return template.replace(/{{\s*(\w+)\s*}}/g, function(_, param) {
-    return obj[param] || "";
-  });
-}
-
-function executeScriptInPageContext(tabId, code) {
-  chrome.tabs.executeScript(tabId, {
-    code: sub(SCRIPT_EXECUTION_TEMPLATE, {code: JSON.stringify(code)})
-  });
-}
-
-function toggleTabConnection(tab, enable) {
-  // @#$%ing security policy requires we insert script contents
-  // that need to access the page variable (e.g. window.io) by
-  // actually inserting a <script> element with the contents as needed...
-  var template = (enable ? LT_WS_CONNECT : LT_WS_DISCONNECT);
-  executeScriptInPageContext(tab.id, sub(template, {host: JSON.stringify('http://localhost:' + port)}));
-}
 
 /**
  * Callback for submitting the popup form
@@ -77,7 +19,7 @@ function configForm_onSubmit(evt) {
  * @param {Event} evt
  */
 function configPort_onChange(evt) {
-  setPort(portField.value);
+  ltconn.setPort(portField.value);
 }
 
 /**
@@ -86,7 +28,14 @@ function configPort_onChange(evt) {
  */
 function configEnable_onChange(evt) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    toggleTabConnection(tabs[0], enableField.checked);
+    var hostname = ltconn.getHostname(tabs[0].url);
+    if (enableField.checked) {
+      ltconn.addHostname(hostname);
+      ltconn.connectToTab(tabs[0].id);
+    } else {
+      ltconn.removeHostname(hostname);
+      ltconn.toggleTabConnection(tabs[0].id, false);
+    }
   });
 }
 
@@ -98,7 +47,9 @@ document.addEventListener('DOMContentLoaded', function() {
   // enable for this domain field
   enableField.addEventListener('change', configEnable_onChange)
 
-  chrome.storage.local.get('port', function(vals) {
-    setPort(vals.port);
+  // sync the form fields with the underlying data
+  portField.value = ltconn.getPort();
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    enableField.checked = ltconn.isHostnameEnabled(ltconn.getHostname(tabs[0].url));
   });
 });
